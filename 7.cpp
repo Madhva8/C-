32fb57dcc469e2a6d04f8afb2ee625137e554eb3
Madhva Fakare <38571235+Madhva8@users.noreply.github.com>
2020-04-13 01:31:40 +0530

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,10 @@
+/*
+String Length
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    string m = "fouerfurnurierhkbfgrfginirnviuriwunvuirnevnrtiunvuirtnvnrtiuvn";
+    cout<<m.length();
+    return 0;
+}
