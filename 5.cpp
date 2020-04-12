6258c8a719c9481f3d67a72d64515ca673f01fa0
Madhva Fakare <38571235+Madhva8@users.noreply.github.com>
2020-04-13 01:10:05 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string Name;
+  int age;
+  cout<<"Your Name:: ";
+  cin>>Name;
+  cout<<"You Age:: ";
+  cin>>age;
+  cout<<"\nHello "<<Name<<", you are "<<age<<" years old.\n";
+  return 0;
+}
