ae023fc23ad1a6841e7d0fa3ee63447069e7d16d
Madhva Fakare <38571235+Madhva8@users.noreply.github.com>
2020-04-13 01:19:44 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,18 @@
+/*
+This is Operator
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter First Number:: ";
+    cin>>x;
+    cout<<"Enter Second Number:: ";
+    cin>>y;
+    cout<<"addition is "<<x+y<<endl;
+    cout<<"subtraction is "<<x-y<<endl;
+    cout<<"multiplication is "<<x*y<<endl;
+    cout<<"division is "<<x/y<<endl;
+    cout<<"mod is "<<x%y<<endl;
+    return 0;
+}
